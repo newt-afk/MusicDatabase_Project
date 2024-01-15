1781924fe914c9e1c31ce5203b70b461abc7e769
zeno <792531@pdsb.net>
2024-01-15 14:42:30 -0500

    cleared out methods from the player class to repurpose it for mainly audio, visual will be handled by the main class.

--- src/Java/Player.java
+++ src/Java/Player.java
@@ -13,19 +13,7 @@ import javafx.scene.Parent;
 import java.io.IOException;
 
 class Player {
-    Boolean randomise;
-    Boolean linkTracks;
 
-    void createVisual() throws IOException {
-        Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/PlayerVisuals.fxml"));
-        Image icon = new Image("Media/podcast-music-microphone-podcasting-desk-plant-social-media-office.png");
-        stage.getIcons().add(icon);
-        stage.setTitle("ZA Music Player");
-        stage.setScene(new Scene(root));
-        stage.show();
-
-    }
     void play() {
 
     }
